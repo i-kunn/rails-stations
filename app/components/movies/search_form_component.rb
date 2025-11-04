75505c790c860814352dcfcc365edbfbf3bd165d
i-kunn <kohei100560@outlook.jp>
2025-11-04 20:45:13 +0900

    'station6一部修正'

--- app/components/movies/search_form_component.rb
+++ app/components/movies/search_form_component.rb
@@ -1,5 +1,10 @@
 class Movies::SearchFormComponent < ViewComponent::Base
-  def initialize(params:)
-    @params = params
+  def initialize(keyword:, is_showing:)
+    @keyword    = keyword
+    @is_showing = is_showing
   end
+
+  def all_selected? = @is_showing.blank?
+  def showing?      = @is_showing == "1"
+  def upcoming?     = @is_showing == "0"
 end
